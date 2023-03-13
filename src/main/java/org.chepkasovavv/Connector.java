d41faa4210e1f3308d7387a6f6108b49bd873ff1
VEsenia <chepkasovavv2@gmail.com>
2023-03-13 17:40:55 +0300

    Работа с базой данных

---
+++ src/main/java/org.chepkasovavv/Connector.java
@@ -0,0 +1,17 @@
+package org.chepkasovavv;
+
+public class Connector {
+
+    private static final Connector instance = new Connector();
+
+    // private constructor to avoid client applications using the constructor
+    private Connector(){}
+
+    public static Connector getInstance() {
+        return instance;
+    }
+
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/users";
+    static final String USER = "tester";
+    static final String PASS = "tester";
+}
